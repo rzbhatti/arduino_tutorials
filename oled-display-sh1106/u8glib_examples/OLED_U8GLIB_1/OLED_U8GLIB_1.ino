bb30b697a642b9c9f0f3ef813ed83187baed51f4
Dr. Rashed Z. Bhatti <rzbhatti@users.noreply.github.com>
2023-05-02 21:31:50 -0700

    Fix for Yellow & Blue Display

--- oled-display-sh1106/u8glib_examples/OLED_U8GLIB_1/OLED_U8GLIB_1.ino
+++ oled-display-sh1106/u8glib_examples/OLED_U8GLIB_1/OLED_U8GLIB_1.ino
@@ -6,7 +6,7 @@ U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); //set i2C Interface
 void draw(void) 
 {
   u8g.setFont(u8g_font_profont15);        // Set display font 1
-  u8g.drawStr(5, 20, "Rasheds.COM"); 
+  u8g.drawStr(5, 5, "Rasheds.COM"); 
   u8g.setFont(u8g_font_profont29);        // Set display font 2
   u8g.setPrintPos(1, 45);                 // set print position 1, 45
   u8g.println("1.3 OLED");         
